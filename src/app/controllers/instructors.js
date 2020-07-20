392ce8972246a4791392802cb478e06b0a99cea1
Victor <victor.fernandes01@hotmail.com>
2020-07-20 11:26:51 -0300

    Diario20

---
+++ src/app/controllers/instructors.js
@@ -0,0 +1,58 @@
+const {age, date} = require('../../lib/utils');
+const Instructor = require('../models/Instructor');
+
+module.exports = {
+    index(req, res) {
+        Instructor.all(function (instructors) {
+            return res.render("instructors/index", {instructors});
+        })
+    },
+
+    create(req, res) {
+        return res.render("instructors/create");
+    },
+
+    post(req, res) {
+        const keys = Object.keys(req.body)
+        for(key of keys) {
+            if(req.body[key] == "") {
+                return res.send('Complete tudo');
+            }
+        }
+
+        Instructor.create(req.body, function (instructor) {
+            return res.redirect(`/instructors/${instructor.id}`);
+        })
+    },
+
+    show(req, res) {
+        Instructor.find(req.params.id, function(instructor) {
+            if (!instructor){
+                return res.send("Instructor not found!");
+            } 
+            
+            instructor.age = age(instructor.birth);
+            instructor.services = instructor.services.split(",");
+            instructor.created_at = date(instructor.created_at).format;
+            return res.render("instructors/show", {instructor});
+        })
+    },
+
+    edit(req, res) {
+        return
+    },
+
+    put(req, res) {
+        const keys = Object.keys(req.body)
+        for(key of keys) {
+            if(req.body[key] == "") {
+                return res.send('Complete tudo');
+            }
+        }
+        return
+    },
+
+    delete(req, res) {
+        return
+    }
+}
